18660008b94dd478f418e0dd63abdf2d6cf05f96
Elena Makovskaya <lenochka.yes91@gmail.com>
2025-05-25 20:05:40 +0300

    hw_lesson_3 added 4 files

---
+++ homework/elena_makovskaya/hw_lesson_3/task3.py
@@ -0,0 +1,11 @@
+#Даны два числа. Найти среднее арифметическое и среднее геометрическое этих чисел
+from math import sqrt
+
+a = 33
+b = 15
+
+arithmetic_mean = (a + b)/2
+geometric_mean = sqrt(a * b)
+
+print(arithmetic_mean)
+print(geometric_mean)
