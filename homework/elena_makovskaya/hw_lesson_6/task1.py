6e5e023f0137a068e0a3f172115d345a8acf814d
Elena Makovskaya <lenochka.yes91@gmail.com>
2025-07-06 17:21:12 +0300

    fixed checks

--- homework/elena_makovskaya/hw_lesson_6/task1.py
+++ homework/elena_makovskaya/hw_lesson_6/task1.py
@@ -7,7 +7,7 @@
 
 
 text = (
-    "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, " +
+    "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, "
     "dignissim vitae libero")
 
 words = text.split()
