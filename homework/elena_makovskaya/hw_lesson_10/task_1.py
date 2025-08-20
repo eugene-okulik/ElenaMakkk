8d4ed0b4755564ed3ceb0aaeb8b112da1381bc95
Elena Makovskaya <elenamakovskaya@MacBook-Air-Elena.local>
2025-08-20 13:12:17 +0300

    tasks for lesson 10

---
+++ homework/elena_makovskaya/hw_lesson_10/task_1.py
@@ -0,0 +1,29 @@
+# Задание на декораторы 1
+# Создайте универсальный декоратор, который можно будет применить к любой функции. Декоратор должен делать следующее:
+# он должен распечатывать слово "finished"после выполнения декорированной функции.
+#
+# Код, использующий этот декоратор может выглядеть, например, так:
+#
+# @finish_me
+# def example(text):
+#     print(text)
+#
+# example('print me')
+# В результате работы будет такое:
+# print me
+# finished
+
+
+def finish_me(func):
+    def wrapper():
+        func()
+        print('finished')
+    return wrapper
+
+
+@finish_me
+def print_me():
+    print('print me')
+
+
+print_me()
